b0fac2c9e9fa110380fbf4908bc468f2e9a7ac1c
prutmongkol <150164864+prutmongkol@users.noreply.github.com>
2025-03-22 00:47:35 +0700

    add progress bar

--- main.gd
+++ main.gd
@@ -5,6 +5,9 @@ extends Control
 @onready var bot_count_label: Label = %BotCountLabel
 @onready var hire_boba_bot_button: Button = %HireBobaBotButton
 @onready var bot_cost_label: Label = %BotCostLabel
+@onready var bot_label: Label = %BotLabel
+@onready var bot_progress_bar: ProgressBar = %BotProgressBar
+
 
 var money = 0
 var bot_count = 0
@@ -13,6 +16,13 @@ var bot_update_rate = 5.0 # in seconds
 var elapsed_time = 0.0
 
 
+func handle_bot(delta: float) -> void:
+	elapsed_time += delta
+	bot_progress_bar.value = elapsed_time / bot_update_rate * 100
+	if elapsed_time >= bot_update_rate:
+		elapsed_time -= bot_update_rate
+		money += bot_count
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,15 +30,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	elapsed_time += delta
-	if elapsed_time >= bot_update_rate:
-		elapsed_time -= bot_update_rate
-		money += bot_count
+	if bot_count > 0:
+		handle_bot(delta)
 		
-	hire_boba_bot_button.disabled = money < bot_cost	
+	hire_boba_bot_button.disabled = money < bot_cost
+	bot_label.visible = bot_count > 0
+	bot_progress_bar.visible = bot_count > 0
 		
 	money_label.text = str(money)
 	bot_count_label.text = str(bot_count) + " bot" if bot_count <= 1 else str(bot_count) + " bots"
+	bot_label.text = str(bot_count) + " cup every 5 seconds" if bot_count <= 1 else str(bot_count) + " cups every 5 seconds"
 
 
 func _on_make_a_drink_button_button_up() -> void:
